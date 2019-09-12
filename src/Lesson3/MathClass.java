20b8b779d774d7cff5c9b8a1cc4f2d9445ad7632
lola1590 <lola1590@ISS2TZNKH2.scdsb.pri>
2019-09-12 11:57:27 -0400

    Work from school

---
+++ src/Lesson3/MathClass.java
@@ -0,0 +1,17 @@
+
+package Lesson3;
+
+
+public class MathClass {
+
+    
+    public static void main(String[] args) {
+        int x =25;
+        System.out.println(Math.pow(x, 3)); //25 pow 3
+        System.out.println(Math.sqrt(x));  //5
+        double y = 8.9876;
+        System.out.println(Math.round(y));  //9
+        System.out.println("The bigger number is " + Math.max(x, y)); //max = highest
+    }
+    
+}
